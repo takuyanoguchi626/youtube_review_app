0167123838331ecb074b3deefc043959bbaa21ae
Mmy26 <yu.b1994masamura@gmail.com>
2022-03-17 16:46:41 +0900

    HTMLの作成

--- src/views/ShowReview.vue
+++ src/views/ShowReview.vue
@@ -1,5 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <div class="content">
+      <div class="account">
+        <div class="icon">
+          <img src="/img/pagu.jpg" />
+        </div>
+        <div class="description">
+          <p>アカウント名</p>
+          <p>登録情報</p>
+          <p>自己紹介</p>
+        </div>
+      </div>
+
+      <div class="cardSize card large">
+        <div class="movie">
+          <img src="/img/tokai3.jpeg" />
+          <p>動画タイトル</p>
+          <div class="evaluation">
+            <img src="/img/hyouka.png" />
+          </div>
+        </div>
+        <div class="review">
+          <p>タイトル</p>
+          <p>
+            あああああああああああああaaaaaaaaaaaaaaaあああああああああああああaaaaaaaaaaaaaaaaaaaaaあああああああああああああああaaaaaaaaaaaaaaaaaaaaaあああああああああああああああaaaaaaaaaaaaaaaaaaaaaああaあああああああああああああaaaaaaaaaaaaaaaaaaaaaあああああああああああああああaaaaaaaaaaaaaaaaaaaaaあああああああああああああああaaaaaaaaaaaaaaaaaaaaaあああああああああああああああaaaaaaaaaaaaaaaaaaaaaあああああああああああああああaaaaaaaaaaaaaaaaaaaaaあああああああああああああああaaaaaaaaaaaaaaaaaaaaaあああああああああああああああaaaaaaaaaaaaaaaaaaaaaああaaaaaああ
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,4 +37,19 @@ import { Component, Vue } from "vue-property-decorator";
 export default class XXXComponent extends Vue {}
 </script>
 
-<style scoped></style>
+<style scoped>
+.content {
+  display: flex;
+  justify-content: center;
+}
+.cardSize {
+  display: flex;
+  justify-content: baseline;
+  height: auto;
+  width: 50%;
+}
+movie {
+  width: 50;
+  height: auto;
+}
+</style>
