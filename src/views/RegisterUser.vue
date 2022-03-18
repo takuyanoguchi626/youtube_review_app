a6f00e23dca0da1bd7c3f5194470e7909bf9963b
Mmy26 <yu.b1994masamura@gmail.com>
2022-03-18 17:37:46 +0900

    登録メソッドの実装

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -4,9 +4,10 @@
       <div class="row register-page">
         <div class="col s12 z-depth-6 card-panel">
           <div class="pageTitle">会員登録</div>
-          <div class="error">エラーメッセージ</div>
+
           <div class="row">
             <div class="input-field col s6">
+              <div class="error">{{ this.lastNameError }}</div>
               <input
                 id="last_name"
                 type="text"
@@ -17,6 +18,7 @@
               <label for="last_name">姓</label>
             </div>
             <div class="input-field col s6">
+              <div class="error">{{ this.firstNameError }}</div>
               <input
                 id="first_name"
                 type="text"
@@ -29,6 +31,7 @@
           </div>
           <div class="row">
             <div class="input-field col s12">
+              <div class="error">{{ this.emailError }}</div>
               <input
                 id="email"
                 type="email"
@@ -42,6 +45,7 @@
 
           <div class="row">
             <div class="input-field col s12">
+              <div class="error">{{ this.passwordError }}</div>
               <input
                 id="password"
                 type="password"
@@ -55,12 +59,14 @@
           </div>
           <div class="row">
             <div class="input-field col s12">
+              <div class="error">{{ this.passwordConfirmError }}</div>
+
               <input
                 id="confirmation_password"
                 type="password"
                 class="validate"
                 minlength="8"
-                v-model="passwordComfirm"
+                v-model="passwordConfirm"
                 required
               />
               <label for="confirmation_password">確認用パスワード</label>
@@ -90,20 +96,80 @@ export default class XXXComponent extends Vue {
   private tel = "";
   private password = "";
   private passwordConfirm = "";
+  private lastNameError = "";
+  private firstNameError = "";
+  private emailError = "";
+  private telError = "";
+  private passwordError = "";
+  private passwordConfirmError = "";
+  private errorChecker = false;
 
   public register(): void {
-    const newAccount = new Account(
-      0,
-      this.lastName + this.firstName,
-      "",
-      this.email,
-      this.tel,
-      this.password,
-      new Array<Channels>(),
-      new Array<Review>()
-    );
-    console.log(newAccount);
-    this.$store.commit("addNewAccount", newAccount);
+    this.lastNameError = "";
+    this.firstNameError = "";
+    this.emailError = "";
+    this.telError = "";
+    this.passwordError = "";
+    this.passwordConfirmError = "";
+    if (this.lastName === "") {
+      this.lastNameError = "姓を入力してください";
+      this.errorChecker = true;
+    }
+    if (this.firstName === "") {
+      this.firstNameError = "名を入力してください";
+      this.errorChecker = true;
+    }
+    if (this.email === "") {
+      this.emailError = "メールアドレスを入力してください";
+      this.errorChecker = true;
+    }
+    if (this.tel === "") {
+      this.telError = "電話番号を入力してください";
+      this.errorChecker = true;
+    }
+    if (this.password === "") {
+      this.passwordError = "パスワードを入力してください";
+      this.errorChecker = true;
+    }
+    if (this.passwordConfirm === "") {
+      this.passwordConfirmError = "確認用パスワードを入力してください";
+      this.errorChecker = true;
+    }
+    if (this.password !== "" && this.password !== this.passwordConfirm) {
+      this.passwordError = "パスワードと確認用パスワードが異なります";
+      this.errorChecker = true;
+    }
+    if (this.errorChecker === true) {
+      this.errorChecker = false;
+      return;
+    }
+    const accountList = this.$store.getters.getAccountList;
+    const accountId = accountList.id;
+    if (accountId === undefined) {
+      const newAccount = new Account(
+        0,
+        this.lastName + this.firstName,
+        "",
+        this.email,
+        this.tel,
+        this.password,
+        new Array<Channels>(),
+        new Array<Review>()
+      );
+      this.$store.commit("addNewAccount", newAccount);
+    } else {
+      const newAccount = new Account(
+        accountList[accountList.length - 1].id + 1,
+        this.lastName + this.firstName,
+        "",
+        this.email,
+        this.tel,
+        this.password,
+        new Array<Channels>(),
+        new Array<Review>()
+      );
+      this.$store.commit("addNewAccount", newAccount);
+    }
   }
 }
 </script>
