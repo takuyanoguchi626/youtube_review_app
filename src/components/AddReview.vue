69a18939bc00bd43d441b86d6092865d1a8152bd
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-03-21 16:44:28 +0900

    レビュー投稿フォームを作成

--- src/components/AddReview.vue
+++ src/components/AddReview.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
     <div>
+      評価：
+      <label for="1">
+        <input type="radio" name="hyouka" id="1" /> <span> 1</span>
+      </label>
+      <label for="2">
+        <input type="radio" name="hyouka" id="2" /> <span> 2</span>
+      </label>
+      <label for="3">
+        <input type="radio" name="hyouka" id="3" /> <span> 3</span>
+      </label>
+      <label for="4">
+        <input type="radio" name="hyouka" id="4" /> <span> 4</span>
+      </label>
+      <label for="5">
+        <input type="radio" name="hyouka" id="5" /> <span> 5</span>
+      </label>
+    </div>
+    <div>
       <textarea name="" id="" cols="30" rows="10"></textarea>
     </div>
     <button>レビューを投稿</button>
