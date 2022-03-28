c160812d2ef74f5dedc291e74a4a2941aa3036b4
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-03-28 12:09:03 +0900

    画面遷移時、ページトップに行くように改修。アカウント画面への画面遷移をIDで紐づけ

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -117,6 +117,27 @@ export default class XXXComponent extends Vue {
   private flag = true;
 
   async created(): Promise<void> {
+    // スクロールトップボタン
+    scrollTop(1); // 遅すぎるとガクガクになるので注意
+
+    function scrollTop(duration: number) {
+      let currentY = window.pageYOffset; // 現在のスクロール位置を取得
+      let step = duration / currentY > 1 ? 10 : 100; // 三項演算子
+      let timeStep = (duration / currentY) * step; // スクロール時間
+      let intervalId = setInterval(scrollUp, timeStep);
+      // timeStepの間隔でscrollUpを繰り返す。
+      // clearItervalのために返り値intervalIdを定義する。
+
+      function scrollUp() {
+        currentY = window.pageYOffset;
+        if (currentY === 0) {
+          clearInterval(intervalId); // ページ最上部に来たら終了
+        } else {
+          scrollBy(0, -step); // step分上へスクロール
+        }
+      }
+    }
+
     /**
      *Vuexストアのアクション経由非同期でWebAPIから急上昇動画Top5を取得する.
      *@returns Promiseオブジェクト
