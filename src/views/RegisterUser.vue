6ce04d734a84ba3246f76b73facd52f1a78a7185
Mmy26 <yu.b1994masamura@gmail.com>
2022-03-28 20:00:07 +0900

    RegisterUser.vue TsDocコメント

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -96,27 +96,45 @@ import { Channels } from "@/types/Channels";
 import { Review } from "@/types/Review";
 @Component
 export default class XXXComponent extends Vue {
+  // 姓
   private lastName = "";
+  // 名前
   private firstName = "";
+  // メールアドレス
   private email = "";
+  // 電話番号
   private tel = "";
+  // パスワード
   private password = "";
+  // 確認用パスワード
   private passwordConfirm = "";
+  // 姓のエラー文
   private lastNameError = "";
+  // 名のエラー文
   private firstNameError = "";
+  // メールアドレスのエラー文
   private emailError = "";
+  // 電話番号のエラー文
   private telError = "";
+  // パスワードのエラー文
   private passwordError = "";
+  // 確認用パスワードのエラー文
   private passwordConfirmError = "";
+  // エラーチェッカー
   private errorChecker = false;
 
+  /**
+   * ユーザー登録情報をstoreに送る.
+   */
   public register(): void {
+    // 空にする
     this.lastNameError = "";
     this.firstNameError = "";
     this.emailError = "";
     this.telError = "";
     this.passwordError = "";
     this.passwordConfirmError = "";
+    // エラーハンドリング
     if (this.lastName === "") {
       this.lastNameError = "姓を入力してください";
       this.errorChecker = true;
@@ -149,8 +167,10 @@ export default class XXXComponent extends Vue {
       this.errorChecker = false;
       return;
     }
+    // 登録されているユーザー情報の取得
     const accountList = this.$store.getters.getAccountList;
     const accountId = accountList.id;
+    // idが既に存在する場合と存在しない場合で新たに付与するidを分ける
     if (accountId === undefined) {
       const newAccount = new Account(
         0,
