ad9711424913555d8101c313ade683050695aea8
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-04-06 10:09:30 +0900

    メソッド記入(エラー発生中)

--- src/components/Twitter.vue
+++ src/components/Twitter.vue
@@ -12,92 +12,32 @@
 </template>
 
 <script>
-// import { Component, Vue } from "vue-property-decorator";
 import firebase from "firebase";
-import { Account } from "@/types/Account";
-// import { Channels } from "@/types/Channels";
-// import { Review } from "@/types/Review";
+
 export default {
-  name: "Google",
-  data() {
-    return {
-      userName: "",
-      email: "",
-      photoURL: "",
-      newAccount: [],
-    };
-  },
+  name: "Signin",
   methods: {
-    onClick: async function () {
-      var provider = new firebase.auth.GoogleAuthProvider();
-      await firebase
+    onClick: function () {
+      var provider = new firebase.auth.TwitterAuthProvider();
+      firebase
         .auth()
         .signInWithPopup(provider)
         .then(
           (result) => {
-            console.log(result);
-            this.userName = result.user.displayName;
-            this.email = result.user.email;
-            this.photoURL = result.user.photoURL;
+            var token = result.credential.token;
+            var secret = result.credential.secret;
+            var user = result.user;
+            if (user) {
+              alert("成功");
+            } else {
+              alert("有効なアカウントではありません");
+            }
           },
-          (error) => {
-            alert("ログインに失敗しました");
+          (err) => {
+            alert(err.message);
           }
         );
-      console.log(this.userName + "/" + this.email + "/" + this.photoURL);
-      // 登録されているユーザー情報の取得
-      const accountLastId = this.$store.getters.getLastUserId;
-      // 現在のアカウントリスト
-      const currentAccountList = this.$store.getters.getAccountList;
-      console.log(currentAccountList);
-      // 新たなユーザーに使用するID
-      let newUserId = 0;
-      // 既にログインしたことのあるアカウントの判別
-      for (let i = 0; i < currentAccountList.length; i++) {
-        if (currentAccountList[i].mailaddless === this.email) {
-          newUserId = currentAccountList[i].id;
-          console.log(newUserId);
-          this.$router.push(`/myProfile/${newUserId}`);
-          this.$store.commit("addCurrentUser", currentAccountList[i]);
-          return;
-        }
-      }
-      // idが既に存在する場合と存在しない場合で新たに付与するidを分ける
-      if (accountLastId === 0) {
-        this.newAccount = new Account(
-          1,
-          this.userName,
-          "",
-          this.photoURL,
-          this.email,
-          "",
-          "",
-          [],
-          []
-        );
-        newUserId = this.newAccount.id;
-        this.$store.commit("addUser", this.newAccount);
-        this.$store.commit("addCurrentUser", this.newAccount);
-        this.$store.commit("addLastUserId", newUserId);
-      } else {
-        this.newAccount = new Account(
-          accountLastId + 1,
-          this.userName,
-          "",
-          this.photoURL,
-          this.email,
-          "",
-          "",
-          [],
-          []
-        );
-        newUserId = this.newAccount.id;
-        this.$store.commit("addUser", this.newAccount);
-        this.$store.commit("addCurrentUser", this.newAccount);
-        this.$store.commit("addLastUserId", newUserId);
-      }
     },
-    // firebase.auth().signOut();
   },
 };
 </script>
